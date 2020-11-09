5c336c252e469fd7ac717d3b0021a66e45937c77
Patrick Chan <q_p18@hotmail.com>
2020-11-09 19:55:46 +0000

    Complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+
+    if(times < 0) {
+        return "ERROR"
+    }
+    if(times === 0 || str === "") {
+        return "";
+    }
+    if(times === 1) {
+        return str
+    }
+    if(times > 1) {
+        let newString = "";
+        for(let i = 0; i < times; i++) {
+            newString = newString.concat(str);
+        }
+        return newString;
+    }
 
 }
 
