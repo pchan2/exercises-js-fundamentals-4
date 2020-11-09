d32a5b34627e61b2bd38ca271ebcb0a7686f1259
Patrick Chan <q_p18@hotmail.com>
2020-11-09 13:42:14 +0000

    Complete leapYear

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (!(year % 400)) {
+        return true;
+    } else if (year % 100 && !(year % 4)) {
+        return true;
+    } return false;
+    
 }
 
 module.exports = leapYears
