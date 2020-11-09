18cccdfce6bd42fbcaffe08d434b0d2ca1c1c64f
Patrick Chan <q_p18@hotmail.com>
2020-11-09 21:00:31 +0000

    Complete sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
 
+    if(typeof numA === "string" || typeof numB === "string" || numA < 0 || numB < 0 || Array.isArray(numA) || Array.isArray(numB)) {
+        return "ERROR";
+    } else if (numA < numB) {
+        let sum = 0;
+        for(let i = numA; i <= numB; i++) {
+            sum = sum + i;
+        }
+        console.log(sum);
+        return sum;
+    } else if (numA > numB) {
+        let sum = 0;
+        for(let i = numB; i <= numA; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    
 }
 
 module.exports = sumAll
