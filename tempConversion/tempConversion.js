157c7c6b77bc1540e2810db6ccd8803033854c2e
Patrick Chan <q_p18@hotmail.com>
2020-11-10 07:05:04 +0000

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  
+  if (temp === 32) {
+    return 0;
+  } else {
+    let tempInCelsius = (temp - 32) * 5 / 9;
+    let roundedTemp = Number.parseFloat(tempInCelsius.toFixed(1));
+    return roundedTemp;
+  }
 }
 
-const ctof = function() {
+const ctof = function(temp) {
 
+  if (temp === 0 ) {
+    return 32;
+  } else {
+    let tempInFahrenheit = temp * 9 / 5 + 32;
+    let roundedTemp = Number.parseFloat(tempInFahrenheit.toFixed(1));
+    return roundedTemp;
+  }
 }
 
 module.exports = {
