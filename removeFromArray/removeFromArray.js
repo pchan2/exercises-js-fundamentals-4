ba9c929d76a9dbb9e50c0dd069d2696da7bb22d3
Patrick Chan <q_p18@hotmail.com>
2020-11-10 08:43:07 +0000

    Complete removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-
-}
+function compareNumbers(a, b) {
+    return a - b;
+  }
+  
+  const removeFromArray = function(arr, ...vals) {
+  
+    vals.sort(compareNumbers);
+      
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < vals.length; j++) {
+            if (arr[i] === vals[j]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
+      
+  }
+  
+  removeFromArray([1, 2, 3, 4], 3, 2)
 
 module.exports = removeFromArray
